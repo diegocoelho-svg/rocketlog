081faeaae20c9cc50291802e47481ac2b2374413
Diego Coelho <diegocoelhoaprova@gmail.com>
2025-08-21 23:52:25 -0300

    chore(tests): remove test user after authentication tests

--- src/tests/sessions-controller.test.ts
+++ src/tests/sessions-controller.test.ts
@@ -1,8 +1,13 @@
 import request from "supertest"
 import { app } from "@/app"
+import { prisma } from "@/database/prisma"
 
 describe("SessionsController", () => {
   let user_id: string
+
+  afterAll(async () => {
+    await prisma.user.delete({ where: { id: user_id } })
+  })
 
   it("should authenticate a and get access token", async () => {
     const userResponse = await request(app).post("/users").send({
